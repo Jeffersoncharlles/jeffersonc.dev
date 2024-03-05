d8a22360ca7472e4f371909e2e07bd8ab923d89b
Jefferson Charlles <contato@jefferdeveloper.com>
2024-03-05 08:11:27 -0300

    feat:animation stacks in tailwindcss

--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -8,24 +8,65 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      "animation": {
-        "flip": "flip 6s infinite steps(2, end)",
-        "rotate": "rotate 3s linear infinite both"
+      animation: {
+        flip: "flip 6s infinite steps(2, end)",
+        rotate: "rotate 3s linear infinite both",
+        reactNative: "stack 7s ease infinite alternate",
+        nodeJs: "node 5s ease  infinite alternate",
+        vscode: "stack 7s ease  infinite alternate",
+        reactJs: "reactjs 7s ease infinite alternate",
+        typescript: "types 7s ease infinite alternate",
       },
-      "keyframes": {
-        "flip": {
+      keyframes: {
+        flip: {
           "to": {
             "transform": "rotate(360deg)"
           }
         },
-        "rotate": {
+        rotate: {
           "to": {
             "transform": "rotate(90deg)"
           }
-        }
-      }
+        },
+        stack: {
+          '0%': {
+            transform: 'translateY(-30px) rotate(0deg)',
+          },
+          '100%': {
+            transform: 'translateY(0px) rotate(0deg)',
+          },
+        },
+        types: {
+          '0%': {
+            transform: 'translateY(10px) scale(0.9)',
+          },
+          '50%': {
+            transform: 'translateY(30px) translateX(-10px)',
+          },
+          '100%': {
+            transform: 'translateY(30px) translateX(-30px) scale(1)',
+          },
+        },
+        node: {
+          '0%': {
+            transform: 'translateY(-30px) scale(1)',
+          },
+          '100%': {
+            transform: 'translateY(10px) scale(0.9)',
+          },
+        },
+        reactjs: {
+          '0%': {},
+          '50%': {
+            transform: 'translateY(-30px) scale(1)',
+          },
+          '100%': {
+            transform: 'translateY(20px) scale(0.9)',
+          },
+        },
+      },
     },
-  },
-  plugins: [],
-};
+    plugins: [],
+  }
+}
 export default config;
