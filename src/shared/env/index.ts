b8b93209060addfb2927b7fb452c586517b36cf1
Jefferson Charlles <contato@jefferdeveloper.com>
2026-04-14 18:35:44 -0300

    fix: payload cookies envs

--- src/shared/env/index.ts
+++ src/shared/env/index.ts
@@ -7,10 +7,10 @@ const envSchema = z.object({
   DATABASE_URL: z.string(),
   PAYLOAD_SECRET: z.string(),
   NEXT_PUBLIC_SERVER_URL: z.string().url('Invalid server URL'),
-  PAYLOAD_COOKIE_DOMAIN: z.string().optional(),
-  PAYLOAD_COOKIE_SAMESITE: z.enum(['Lax', 'Strict', 'None']).default('Lax'),
-  PAYLOAD_COOKIE_SECURE: z.boolean().optional(),
-  PAYLOAD_COOKIE_PREFIX: z.string().default('payload'),
+  // PAYLOAD_COOKIE_DOMAIN: z.string().optional(),
+  // PAYLOAD_COOKIE_SAMESITE: z.enum(['Lax', 'Strict', 'None']).default('Lax'),
+  // PAYLOAD_COOKIE_SECURE: z.boolean().optional(),
+  // PAYLOAD_COOKIE_PREFIX: z.string().default('payload'),
 })
 
 export const env = envSchema.parse(process.env)
