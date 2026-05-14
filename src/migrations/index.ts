1aa6c33da64131372395d4cd683a81ffbb3c331f
Jefferson Charlles <contato@jefferdeveloper.com>
2026-05-14 09:16:06 -0300

    chore: ci pull request migration update

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
-import * as migration_20260410_213830 from './20260410_213830';
+import * as migration_20260410_213830 from './20260410_213830'
+import * as migration_20260514_121349 from './20260514_121349'
 
 export const migrations = [
   {
     up: migration_20260410_213830.up,
     down: migration_20260410_213830.down,
-    name: '20260410_213830'
+    name: '20260410_213830',
   },
-];
+  {
+    up: migration_20260514_121349.up,
+    down: migration_20260514_121349.down,
+    name: '20260514_121349',
+  },
+]
